8311997e9a531546b6ac0c581005a71b3a6561d2
alan davoust <alandavoust@gmail.com>
2016-01-20 23:41:27 -0500

    view updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blogposts
+  root 'blogposts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
