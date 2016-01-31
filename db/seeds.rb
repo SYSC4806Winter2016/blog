a5b3f2a8f05e2ebd7dcb173688edce09b5069e16
alan davoust <alandavoust@gmail.com>
2016-01-31 11:20:48 -0500

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CORPUSFILE = "#{Rails.root}/db/brown_sample.txt"
+DictionaryWord.destroy_all
+puts "destroyed all existing records -- "
+sp = Spellchecker.new(CORPUSFILE)
+my_dictionary = sp.dictionary
+puts "trained dict -- "
+
+ActiveRecord::Base.transaction do #wrap this stuff in a transaction  
+  my_dictionary.each do |w,c|
+    DictionaryWord.create(word: w, count: c) 
+  end
+end
+
+puts "Inserted #{DictionaryWord.count} words"
